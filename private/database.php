d771a990acf563d5d1a4af059eab1ea3c97db6e0
PisethSun <pisethsun92@gmail.com>
2024-02-28 01:45:29 -0500

    add db function

--- private/database.php
+++ private/database.php
@@ -1,9 +1,9 @@
 <!-- Store all function  -->
 
-<php 
-require_once("db_credentials.php");
+<?php 
+require_once('db_credentials.php');
 function db_connect(){
-
-    
+    $connection = mysqli_connect(DB_SERVER, DB_NAME,DB_USER, DB_PASS);
+    return $connection;
 }
 ?>
