f8ef36491d5f650038b456683a4521249d7b21b7
PisethSun <pisethsun92@gmail.com>
2024-03-24 12:31:33 -0400

    remove

--- public/users/index.php
+++ public/users/index.php
@@ -1,7 +1,8 @@
 <?php require_once('../../private/initialize.php');?>
-<?php require_login();?>
+
 <?php $page_title = 'Welcome -Book Appointment Today';?>
 <?php include(SHARED_PATH .'/user_header.php');?>
+
 
 
 
